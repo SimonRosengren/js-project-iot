6e97b409092df6779edd6d61939ac3776584514b
Sara Ranstorp <sara@ranstorp.se>
2018-05-22 14:15:21 +0200

    CSS-changes

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ function GetWidth(){
 window.onresize =  function(){
   humidityChart.options.width = GetWidth();
   console.log(humidityChart.options);
-  
+
   humidityChart.draw();
 }
 
@@ -344,4 +344,25 @@ var endpoint = createEndpoint(
       document.body.style.color = "#000";
    }*/
 
-   document.body.style.backgroundColor = "#333333";
+   // document.body.style.backgroundColor = "#333333";
+
+
+
+   // CHECKBOXAR
+
+   $('input[type=checkbox]').change(function(){
+     counter = 0;
+     clicked = $(this).data('index');
+     $('input[type=checkbox]').each(function(){
+       if($(this)[0].checked){
+         counter++;
+       }
+     });
+     if(counter==3){
+       toDisable = clicked;
+       while(toDisable==clicked){
+         toDisable=Math.round(Math.random()*2);
+       }
+       $("input:eq("+toDisable+")")[0].checked = false;
+     }
+   });
