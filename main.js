fd2b3c838ecad943d3f63c29600a4cc392b19621
antonidag <antonidag@hotmail.com>
2018-05-22 19:34:35 +0200

    Fixed checkboxes

--- main.js
+++ main.js
@@ -354,20 +354,17 @@ var endpoint = createEndpoint(
 
 
    // CHECKBOXAR
-
    $('input[type=checkbox]').change(function(){
-     counter = 0;
-     clicked = $(this).data('index');
-     $('input[type=checkbox]').each(function(){
-       if($(this)[0].checked){
-         counter++;
-       }
-     });
-     if(counter==3){
-       toDisable = clicked;
-       while(toDisable==clicked){
-         toDisable=Math.round(Math.random()*2);
-       }
-       $("input:eq("+toDisable+")")[0].checked = false;
-     }
-   });
+    var counter = 0;
+    $('input[type=checkbox]').each(function(){
+      if($(this)[0].checked){
+        counter++;
+      }
+    });
+    if(counter==3){
+      setTimeout(()=>{
+        $(this)[0].checked = false;
+      },500)
+    }
+  });
+
